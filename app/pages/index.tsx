e7837fa51e4d9f2519d43898cac3c9689220dca2
Arpad Borsos <arpad.borsos@googlemail.com>
2018-11-13 15:54:17 +0100

    importing file outside app root

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,9 +1,15 @@
-import Link from 'next/link'
-import Layout from '../components/Layout';
+import Link from "next/link";
+import Layout from "../components/Layout";
+import foo from "../../foo";
 
 export default () => (
   <Layout title="Home | Next.js + TypeScript Example">
+    {foo("foo")}
     <h1>Hello Next.js 👋</h1>
-    <p><Link href='/about'><a>About</a></Link></p>
+    <p>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+    </p>
   </Layout>
-)
+);
---
+++ error.md
@@ -0,0 +1,10 @@
+```
+{ Error: (client) ../foo.ts 1:0
+Module parse failed: The keyword 'interface' is reserved (1:0)
+You may need an appropriate loader to handle this file type.
+> interface Foo {
+|   prop: string;
+| }
+ @ ./pages/index.tsx 4:0-28 8:5-8
+ @ multi ./pages/index.tsx
+```
